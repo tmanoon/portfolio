a09a4d9f3983e9da796c634b7a98575317c3565a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-01 23:43:55 +0300

    Add basic structure to app header

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
+    const pagesName: string[] = ['about', 'home', 'projects', 'resume']
     return (
     <header>
-        
+        <ul>
+            {pagesName.map(page => {
+                return (
+                <li className="page-link">
+                    <Link to={`${page}`}>{page}</Link>
+                </li>
+                )
+            })}
+        </ul>
     </header>
     )
 }
