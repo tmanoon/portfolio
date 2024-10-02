765a62b877f2d43f8280b3a02fe9b34dc9562464
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-03 02:17:52 +0300

    Edit structure of header component

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom"
 const Header = () => {
     const pagesName: string[] = ['about', 'home', 'projects', 'resume']
     return (
-    <header>
-        <ul>
+    <header className="flex">
+        <button className="header-hamburger">
+
+        </button>
+        <ul className="links flex">
             {pagesName.map(page => {
                 return (
                 <li className="page-link">
-                    <Link to={`${page}`}>{page}</Link>
+                    <Link to={`/${page}`}>{page}</Link>
                 </li>
                 )
             })}
