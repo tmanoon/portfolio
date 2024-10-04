319fde4d29ccb5d8bae613bb372b93c91d62179d
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-04 06:39:40 +0300

    Edit structure of header component

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom"
 
+
 const Header = () => {
+
+    type Page = {
+        pageName: string,
+        icon: string
+    }
+
     const pagesName: string[] = ['about', 'home', 'projects', 'resume']
 
     return (
-        <header className="flex space-between">
-            <div className="logo">
-                <h1>Shoval.S;</h1>
+        <header>
+            <div className="logo-and-nav flex">
+                <div className="logo">
+                    <h1>Shoval.S<span>;</span></h1>
+                </div>
+                <nav className="header-nav-links">
+                    <ul className="links flex">
+                        {pagesName.map(page => {
+                            return (
+                                <li className="page-link">
+                                    <Link to={`/${page}`}>{page}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
             </div>
-            <nav className="expanded-links-nav">
+            <nav className="menu-links-nav">
                 <ul className="links grid">
-                    {pagesName.map(page => {
-                        return (
-                            <li className="page-link">
-                                <Link to={`/${page}`}>{page}</Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-            <button className="header-hamburger">
-                <span className="ham-line"></span>
-                <span className="ham-line"></span>
-                <span className="ham-line"></span>
-            </button>
-            <nav className="header-nav-links">
-                <ul className="links flex">
                     {pagesName.map(page => {
                         return (
                             <li className="page-link">
