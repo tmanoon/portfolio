5e016e40e28ba412986e8686a6ddbafa5da0afb6
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-07 19:14:08 +0300

    Edit structure of home page

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 const Home = () => {
     return (
         <section className="home">
-            <div className="txt-and-img grid">
+            <div className="txt-and-img grid center">
                 <div className="txt grid center">
                     <h1>Shoval Sabag</h1>
                     <h3>Full Stack Developer</h3>
-                    <p>From pixel to server, driven by a relentless pursuit of excellence in web development</p>
+                    <p>From pixel to server, <br />
+                        driven by a relentless pursuit of excellence in web development</p>
                 </div>
-                {/* <img src='/src/assets/pics/girl-coding.png' /> */}
+                <img src='/src/assets/pics/girl-coding.png' />
             </div>
         </section>
     )
