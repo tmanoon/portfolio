78ad24468c6e9302840d8d3e81fabab3f448a2a8
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-10 01:45:26 +0300

    Add basic structure to about page

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -1,8 +1,32 @@
 const About = () => {
+
+    const stackLangs = ['html', 'css', 'javascript']
+    const stackLibs = ['scss', 'typescript', 'react', 'redux', 'vue', 'vuex', 'angular',
+     'rxss', 'nodejs', 'express', 'babel', 'socketIO']
+    const stackDatabases = ['mongo', 'sql']
+    const stackTechs = ['git', 'vscode', 'jira', 'trello', 'monday', 'restfulAPI']
+
     return (
-        <div>
-            hello
-        </div>
+        <section className="about">
+            <div className="txt-and-stack grid">
+                <div className="txt flex column align-center">
+                    <h1>About</h1>
+                    <p>I’m Shoval Sabag, a Full-Stack Developer focused on building efficient, user-friendly web applications.
+                        <br />
+                        Proficient in HTML, CSS, JavaScript, and frameworks like React, Angular, Node.js and more, I create dynamic, responsive, and scalable solutions.
+                        <br />
+                        My experience with RESTful APIs enables me to build reliable backend systems,
+                        efficiently handling API requests and delivering responses with data from MongoDB or SQL databases.
+                        <br />
+                        Fluent in Hebrew and English, and proficient in Russian and Spanish, I collaborate effectively with diverse teams to ensure smooth project delivery.
+                        <br />
+                        Let’s connect to develop innovative solutions that drive success.</p>
+                </div>
+                <div className="stack">
+                
+                </div>
+            </div>
+        </section>
     )
 }
 
