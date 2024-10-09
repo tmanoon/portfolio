6992c1c0e00c5319f17fe155f17609642218d93c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-09 13:17:45 +0300

    Add contact page route in app component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,30 +1,31 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import Header  from './components/Header';
+import Header from './components/Header';
 import Footer from './components/Footer';
+import Socials from './components/Socials';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
 import About from './pages/About';
 import Projects from './pages/Projects';
-
+import Contact from './pages/Contact';
 import './style/main.css'
-import Socials from './components/Socials';
 
 function App() {
 
   return (
     <Router>
-    <Header />
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/resume' element={<Resume />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
-    </main>    
-    <Socials />
-    <Footer />
-  </Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </main>
+      <Socials />
+      <Footer />
+    </Router>
   )
 }
 
