df2a2de510061b3102d52f92f4c510b5857db4c5
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-23 19:57:27 +0300

    Edit structure of resume page

--- portfolio/src/pages/Resume.tsx
+++ portfolio/src/pages/Resume.tsx
@@ -1,19 +1,23 @@
 import { useState } from "react"
 
 const Resume = () => {
-    const [cvLang, changeCvLang] = useState({lang: 'english', btnTxt: 'Download CV'})
+    const [cvLang, changeCvLang] = useState({ lang: 'english', btnTxt: 'Download CV' })
 
     const changeLang = (lang: string) => {
-        if (lang === 'heb') changeCvLang(() => ({ lang: 'עברית', btnTxt: 'הורד קורות חיים' }))
+        if (lang === 'heb') changeCvLang(() => ({ lang: 'hebrew', btnTxt: 'הורד קורות חיים' }))
         else changeCvLang(() => ({ lang: 'english', btnTxt: 'Download CV' }))
     }
 
     return (
-        <section className="resume">
+        <section className="resume flex column">
             <div className="lang-btns">
                 <button className="heb-btn" onClick={() => changeLang('heb')}>עברית</button>
                 <button className="eng-btn" onClick={() => changeLang('eng')}>English</button>
             </div>
+            <img src={`/src/assets/pics/cv-${cvLang.lang}.png`}
+                alt={cvLang.lang === 'hebrew' ? 'קורות החיים של שובל סבג' : `Shoval Sabag's resume`}
+                title={cvLang.lang === 'hebrew' ? 'קורות החיים של שובל סבג' : `Shoval Sabag's resume`}
+            />
             <button className="download-btn">{cvLang.btnTxt}</button>
         </section>
     )
