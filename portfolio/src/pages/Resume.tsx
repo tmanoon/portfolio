9f122a08c3bb56613ecd393338ea33ebe9178d7b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-23 19:40:30 +0300

    Create basic structure of resume page

--- portfolio/src/pages/Resume.tsx
+++ portfolio/src/pages/Resume.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
+
 const Resume = () => {
+    const [cvLang, changeCvLang] = useState({lang: 'english', btnTxt: 'Download CV'})
+
+    const changeLang = (lang: string) => {
+        if (lang === 'heb') changeCvLang(() => ({ lang: 'עברית', btnTxt: 'הורד קורות חיים' }))
+        else changeCvLang(() => ({ lang: 'english', btnTxt: 'Download CV' }))
+    }
+
     return (
-        <div>
-            hello
-        </div>
+        <section className="resume">
+            <div className="lang-btns">
+                <button className="heb-btn" onClick={() => changeLang('heb')}>עברית</button>
+                <button className="eng-btn" onClick={() => changeLang('eng')}>English</button>
+            </div>
+            <button className="download-btn">{cvLang.btnTxt}</button>
+        </section>
     )
 }
 
