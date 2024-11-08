c5f3e5ed13b5a459b578ef897fd8a4885c536968
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-11-08 23:44:44 +0200

    Add basic structure of contact page

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -1,6 +1,16 @@
 const Contact = () => {
     return (
-        <section className="contact">hi</section>
+        <section className="contact">
+            <h1>Contact</h1>
+            <form className="flex column">
+                <label htmlFor="fullname">Full Name</label>
+                <input id="fullname" placeholder="Full Name" />
+                <label htmlFor="fullname">Email</label>
+                <input id="email" placeholder="Email" />
+                <label htmlFor="msg"></label>
+                <textarea name="msg" id="msg"></textarea>
+            </form>
+        </section>
     )
 }
 
