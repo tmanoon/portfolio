328c23994f7915c44502e0f10f405c0614a74f39
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-11-17 23:51:53 +0200

    Create field type and make code more readable and shorter in contact page

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -1,15 +1,43 @@
 const Contact = () => {
+
+    type Field = {
+        name: string,
+        label: string
+        placeholder: string
+    }
+
+    const fields: Field[] = [
+        {
+            name: 'fullname',
+            label: 'Full Name',
+            placeholder: 'Full Name'
+        },
+        {
+            name: 'email',
+            label: 'Email',
+            placeholder: 'Email'
+        },
+        {
+            name: 'phone',
+            label: 'Phone (Optional)',
+            placeholder: 'Phone'
+        },
+    ]
+
     return (
         <section className="contact">
             <h1>Contact</h1>
             <form className="flex column align-center" data-netlify="true">
-                <label htmlFor="fullname">Full Name</label>
-                <input id="fullname" placeholder="Full Name" />
-                <label htmlFor="fullname">Email</label>
-                <input id="email" placeholder="Email" />
-                <label htmlFor="phone">Phone (Optional)</label>
-                <input id="phone" placeholder="Phone" />
-                <label htmlFor="msg">Your Message</label>
+                {
+                    fields.map(field => {
+                        return (
+                            <>
+                                <label htmlFor={field.name}>{field.label}</label>
+                                <input id={field.name} placeholder={field.placeholder} />
+                            </>
+                        )
+                    })
+                }
                 <textarea name="msg" id="msg" placeholder="Type your message"></textarea>
                 <button type="button">Submit</button>
             </form>
