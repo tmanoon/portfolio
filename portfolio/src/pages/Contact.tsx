07a6b2d233b4eb62246a62fb560568cd3f9233c7
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-11-17 23:53:04 +0200

    Return label to textarea in contact page

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -38,6 +38,7 @@ const Contact = () => {
                         )
                     })
                 }
+                <label htmlFor="msg">Your Message</label>
                 <textarea name="msg" id="msg" placeholder="Type your message"></textarea>
                 <button type="button">Submit</button>
             </form>
