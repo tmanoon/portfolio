fcf8e3fbaf0793ed10bb776d52620532aa2acbdc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-11-19 17:40:58 +0200

    Create project type in projects page

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -1,4 +1,12 @@
 const Projects = () => {
+    
+    type Project = {
+        name: string,
+        imgUrl: string,
+        desc: string,
+        link: string
+    }
+
     return (
         <section className="projects">
             <h1>Projects</h1>
