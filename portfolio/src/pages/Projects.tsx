c754423873cb106408db3ad8b9386b366ce861a4
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-11-25 00:46:17 +0200

    Add appsus to projects in projects page

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -10,22 +10,27 @@ const Projects = () => {
         {
             name: 'staybnb',
             desc: '',
-            link: 'https://staybnb-1.onrender.com/#/?txt=&entryDate=&exitDate=&label=&placeType=any&pagination=30&adults=0&children=0&infants=0&pets=0&min=0&max=2000&bedrooms=any&beds=any&bathrooms=any&instant=false&selfCheckIn=false&allowsPets=false'  
+            link: 'https://staybnb-1.onrender.com/#/?txt=&entryDate=&exitDate=&label=&placeType=any&pagination=30&adults=0&children=0&infants=0&pets=0&min=0&max=2000&bedrooms=any&beds=any&bathrooms=any&instant=false&selfCheckIn=false&allowsPets=false'
         },
         {
             name: 'manook',
             desc: '',
-            link: 'https://tmanoon.github.io/manook/'  
+            link: 'https://tmanoon.github.io/manook/'
         },
         {
             name: 'form-generator',
             desc: '',
-            link: 'https://tmanoon.github.io/form-generator/'  
+            link: 'https://tmanoon.github.io/form-generator/'
         },
         {
             name: 'ui-challenge',
             desc: '',
-            link: 'https://github.com/tmanoon/frontend-UITask'  
+            link: 'https://github.com/tmanoon/frontend-UITask'
+        },
+        {
+            name: 'appsus',
+            desc: '',
+            link: 'https://tmanoon.github.io/AppSus/'
         },
     ]
 
@@ -33,20 +38,20 @@ const Projects = () => {
         <section className="projects">
             <h1>Projects</h1>
             <ul className="container grid">
-               {
-                projects.map(project => {
-                    return (
-                        <li className="project">
-                            <h3>{(project.name).replaceAll('-', ' ')}</h3>
-                            <img src={`/src/assets/pics/projects/${project.name}.png`} alt={project.name} title={project.name} />
-                            <p>{project.desc}</p>
-                            <a href={project.link}>Move to project</a>
+                {
+                    projects.map(project => {
+                        return (
+                            <li className="project">
+                                <h3>{(project.name).replaceAll('-', ' ')}</h3>
+                                <img src={`/src/assets/pics/projects/${project.name}.png`} alt={project.name} title={project.name} />
+                                <p>{project.desc}</p>
+                                <a href={project.link}>Move to project</a>
                             </li>
-                    )
-                })
-            } 
+                        )
+                    })
+                }
             </ul>
-            
+
         </section>
     )
 }
