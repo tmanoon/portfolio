107256abe5fe0fd78ccf60a6ea95d715c7c9b1d1
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-12-03 14:20:50 +0200

    Edit my about description in about page

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
             <div className="txt-and-stack grid">
                 <div className="txt flex column align-center">
                     <h1>About</h1>
-                    <p>I’m Shoval Sabag, a Full-Stack Developer focused on building efficient, user-friendly web applications.
+                    <p>I’m Shoval, a Full-Stack Developer focused on building efficient, user-friendly web applications.
                         <br />
                         Proficient in HTML, CSS, JavaScript, and frameworks like React, Angular, Node.js and more, I create dynamic, responsive, and scalable solutions.
                         <br />
@@ -26,7 +26,9 @@ const About = () => {
                         <br />
                         Fluent in Hebrew and English, and proficient in Russian and Spanish, I collaborate effectively with diverse teams to ensure smooth project delivery.
                         <br />
-                        Let’s connect to develop innovative solutions that drive success.</p>
+                        Whenever I'm not working on new projects, I take care and play with my two little parrots ♡
+                        <br/>
+                        Let’s connect to develop innovative solutions that drive success!</p>
                 </div>
                 <div className="stack">
                     <ul className="tech-icons grid space-between">
