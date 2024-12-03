5ba09d8dc64903211f1015866d215d481a2ffccd
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-12-03 16:07:52 +0200

    edit functionality of pics in home page

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,23 +1,23 @@
 import { useState } from "react"
 
 const Home = () => {
-    const [selectedPic, setSelectedPic] = useState('me')
+    const [selectedPic, setSelectedPic] = useState(1)
 
     function changePic() {
-        if(selectedPic === 'me') setSelectedPic('me-edited')
-        else setSelectedPic('me')
+        if(selectedPic === 5) setSelectedPic(1)
+        else setSelectedPic(selectedPic + 1)
     }
 
     return (
         <section className="home">
-            <div className="txt-and-img grid">
-                <div className="txt grid center">
+            <div className="txt-and-img grid justify-center">
+                <div className="txt grid justify-center">
                     <h1>Shoval Sabag</h1>
                     <h3>Full Stack Developer</h3>
                     <p>From pixel to server, <br />
                         driven by a relentless pursuit of excellence in web development</p>
                 </div>
-                <img onClick={() => changePic()} src={`/src/assets/pics/${selectedPic}.jpg`} />
+                <img onClick={() => changePic()} src={`/src/assets/pics/me/${selectedPic}.jpg`} />
             </div>
         </section>
     )
