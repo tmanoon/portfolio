ae41bbd55b1251ac74d01531c021a1f3f6d9afe8
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-12-06 00:41:37 +0200

    Edit text in about page

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
                         <br />
                         Fluent in Hebrew and English, and proficient in Russian and Spanish, I collaborate effectively with diverse teams to ensure smooth project delivery.
                         <br />
-                        Whenever I'm not working on new projects, I take care and play with my two little parrots ♡
+                        Whenever I'm not working on new projects, I take care and play with my two little parrots 💜
                         <br/>
                         Let’s connect to develop innovative solutions that drive success!</p>
                 </div>
