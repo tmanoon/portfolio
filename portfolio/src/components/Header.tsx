d15a0c0759464990ca585eb83ef4c3a87b55a055
tmanoon <151874385+tmanoon@users.noreply.github.com>
2025-01-19 23:35:16 +0200

    Change link to a element in order to work in spa structure

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,25 +1,19 @@
-import { Link, useNavigate } from "react-router-dom"
-
-
-
 const Header = () => {
-    const navigate = useNavigate()
 
     const pagesName: string[] = ['home', 'about', 'projects','resume', 'contact']
 
     return (
         <header>
             <div className="logo-and-nav flex">
-                <div className="logo" onClick={() => navigate('/')}>
-                    <img src={`src/assets/pics/logo.png`} alt='logo' title='logo' />
+                <div className="logo">
+                    <a href="home"><img src={`src/assets/pics/logo.png`} alt='logo' title='logo' /></a>
                 </div>
                 <nav className="header-nav-links">
                     <ul className="links flex">
                         {pagesName.map(page => {
                             return (
                                 <li className="page-link flex center" key={page}>
-
-                                    <Link to={`${page === 'home' ? '/' : page}`}>{page}</Link>
+                                    <a href={`#${page}`}>{page}</a>
                                 </li>
                             )
                         })}
@@ -31,7 +25,7 @@ const Header = () => {
                     {pagesName.map(page => {
                         return (
                             <li className="page-link" key={page}>
-                                <Link to={`${page === 'home' ? '/' : page}`}>{page}</Link>
+                                <a href={`#${page}`}>{page}</a>
                             </li>
                         )
                     })}
